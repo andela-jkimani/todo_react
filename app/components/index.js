1e743cf3d1b8ee1811dcf7edb73a69eac888c5f6
andela-jkimani <jackline.kimani@andela.com>
2017-03-07 18:43:49 +0300

    Add first component

--- app/components/index.js
+++ app/components/index.js
@@ -1,1 +1,14 @@
-console.log('Hello Jacky');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
