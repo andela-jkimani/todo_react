b3551349a6005c35edaf250419ca53e47bcb0896
andela-jkimani <jackline.kimani@andela.com>
2017-03-07 18:18:47 +0300

    add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,10 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
 module.exports = {
   entry: './app/components/index.js',
   output: {
@@ -7,5 +14,6 @@ module.exports = {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader' }
     ]
-  }
+  },
+  plugins: [HtmlWebpackPluginConfig]
 };
